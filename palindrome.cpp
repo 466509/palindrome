61ae8f14e1f6dde9275698c02942e668912448ba
U-BSD\466509 <466509@SHS-F5W2NW3.beaverton.k12.or.us>
2026-09-08 10:20:42 -0700

    edits

--- palindrome.cpp
+++ palindrome.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
+#include <cstring>
 
 using namespace std;
 
 int main() {
+  char input[81];
+  char text[81];
+  cout << "Enter some text: ";
+  cin.getline(input, 81);
+  strcpy (text, input);
+  for (int i=0, i < strlen(input), i++) {
+    if (input[i] != isalpha()) {
+      text.erase(i, 1);
+    }
+  }
+  cout << input << endl;
+  cout << text << endl;
   return 0;
 }
