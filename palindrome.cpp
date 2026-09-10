622903b20bd1188833e493a713b1560bf8bd5573
U-BSD\466509 <466509@SHS-F5W2NW3.beaverton.k12.or.us>
2026-09-10 09:57:05 -0700

    Finished no comments yet

--- palindrome.cpp
+++ palindrome.cpp
@@ -1,20 +1,39 @@
 #include <iostream>
 #include <cstring>
+#include <cctype>
 
 using namespace std;
 
 int main() {
-  char input[81];
-  char text[81];
+  char input[81] = "";
+  char text[81] = "";
   cout << "Enter some text: ";
   cin.getline(input, 81);
-  strcpy (text, input);
-  for (int i=0, i < strlen(input), i++) {
-    if (input[i] != isalpha()) {
-      text.erase(i, 1);
+
+  int j=0;
+  for (int i=0; i < strlen(input); i++) {
+    if (isalpha(input[i])) {
+      text[j++] = tolower(input[i]);
     }
   }
-  cout << input << endl;
-  cout << text << endl;
+  text[strlen(text)] = '\0';
+
+  char reverse[81] = "";
+  strcpy(reverse, text);
+  int start=0;
+  int end = strlen(text)-1;
+  while (start < end) {
+    char ch = text[start];
+    reverse[start] = reverse[end];
+    reverse[end] = ch;
+    start++;
+    end--;
+  }
+
+  if (strcmp(text, reverse) == 0) {
+    cout << "Palindrome" << endl;
+  } else {
+    cout << "Not a palindrome" << endl;
+  }
   return 0;
 }
